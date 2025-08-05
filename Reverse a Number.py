418e4dfc4052ebfd40fc245f37b066063646cc4d
Anupam Yadav (Anurag) <anuragyadavanupam@gmail.com>
2025-08-05 22:44:14 +0530

    Add files via upload

---
+++ Reverse a Number.py
@@ -0,0 +1,10 @@
+num = 4562  
+rev_num = 0 
+rev_num = rev_num *10 + num%10 = 2  
+num = num/10 = 456 
+rev_num = rev_num *10 + num%10 = 20 + 6 = 26  
+num = num/10 = 45 
+rev_num = rev_num *10 + num%10 = 260 + 5 = 265  
+num = num/10 = 4 
+rev_num = rev_num *10 + num%10 = 2650 + 4 = 2654  
+num = num/10 = 0
